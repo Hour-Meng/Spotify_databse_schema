d5af245936d3d69d0f9710fd1cd766fc5401d3d5
Hour-Meng <hourmeng.eang26@kit.edu.kh>
2026-03-10 10:51:46 +0700

    version 1

--- main.sql
+++ main.sql
@@ -1,29 +1,29 @@
 # Write main code here
 create table users(
     user_id int PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    gender CHAR(1),
-    password VARCHAR(100) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    country VARCHAR(50),
-    date_of_birth DATE,
-    profile_image BLOB,
-    created_at TIMESTAMP,
-    is_active BOOLEAN,
-    description TEXT
+    u_name VARCHAR(50) NOT NULL,
+    u_gender CHAR(1),
+    u_password VARCHAR(100) NOT NULL,
+    u_email VARCHAR(50) NOT NULL UNIQUE,
+    u_country VARCHAR(50),
+    u_date_of_birth DATE,
+    u_profile_image BLOB,
+    u_created_at TIMESTAMP,
+    u_is_active BOOLEAN,
+    u_description TEXT
 )
 
 
 create Table artists(
     artist_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    gender CHAR(1),
-    date_of_birth DATE,
-    country VARCHAR(50),
-    genre VARCHAR(50),
-    profile_image BLOB,
+    a_name VARCHAR(50) NOT NULL UNIQUE,
+    a_email VARCHAR(50) NOT NULL UNIQUE,
+    a_password VARCHAR(100) NOT NULL,
+    a_gender CHAR(1),
+    a_date_of_birth DATE,
+    a_country VARCHAR(50),
+    a_genre VARCHAR(50),
+    a_profile_image BLOB,
 
-    description TEXT
+    a_description TEXT
 )
